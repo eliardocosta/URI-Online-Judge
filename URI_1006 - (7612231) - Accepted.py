e0d147d1394b8e1f96396bd063c05c21f6adb919
Eliardo Costa <eliardo@ime.usp.br>
2017-07-30 17:36:51 -0300

    Add file via upload

---
+++ URI_1006 - (7612231) - Accepted.py
@@ -0,0 +1,10 @@
+def main(args):
+	A =  float(input())
+	B =  float(input())
+	C =  float(input())
+	M = (2*A + 3*B + 5*C)/10
+	print('MEDIA = %.1f' % M)
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
