cb2918c72722459eadefcf267556dca7991a28d2
Eliardo Costa <eliardo@ime.usp.br>
2017-07-31 10:54:29 -0300

    Add file via upload

---
+++ URI_1007 - (7612256) - Accepted.py
@@ -0,0 +1,11 @@
+def main(args):
+	A =  int(input())
+	B =  int(input())
+	C =  int(input())
+	D =  int(input())
+	D =  A*B - C*D
+	print('DIFERENCA = %d' % D)
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
